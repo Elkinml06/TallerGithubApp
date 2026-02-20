8154bbb303043149e315c2d50a468ee006e1501c
Elkin <elkin1506ml@gmail.com>
2026-02-20 09:55:03 -0500

    feat:agregar datos personales, agregar descripcion de interese, agregar buttom

--- app/src/main/java/me/elkinmendoza/tallergithub/MainActivity.kt
+++ app/src/main/java/me/elkinmendoza/tallergithub/MainActivity.kt
@@ -6,15 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -38,9 +45,25 @@ class MainActivity : ComponentActivity() {
     showSystemUi = true)
 @Composable
 fun presentacion() {
-    Column() {
+    Column(
+
+        modifier = Modifier
+            .fillMaxSize().padding(25.dp)
+        ,
+
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         val painter = painterResource(id = R.drawable.person)
-        Image(painter = painter,contentDescription =null)
+        Image(
+            painter = painter,
+            contentDescription =null,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+                .border(3.dp, Color(0xFF2F80ED), CircleShape)
+                .fillMaxWidth()
+
+        )
 
         Text(
             text = "Elkin Mendoza",
@@ -52,13 +75,103 @@ fun presentacion() {
                 .padding(top = 20.dp)
         )
         Text(
-            text = "Ingeniero de Sistemas",
+            text = "Desarrollador Móvil Senior",
+            color = Color.Blue,
             fontSize = 20.sp,
             textAlign = TextAlign.Center,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 5.dp)
         )
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = Color.Gray,
+            modifier = Modifier.padding(25.dp,35.dp,25.dp,15.dp)
+        )
+        Text(
+            text = "EDAD",
+            fontSize = 20.sp,
+            color = Color.Gray,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp,0.dp)
+        )
+        Text(
+            text = "20 años",
+            fontSize = 20.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp,0.dp)
+        )
+        Text(
+            text = "CORREO",
+            fontSize = 20.sp,
+            color = Color.Gray,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp,15.dp,25.dp,0.dp)
+        )
+        Text(
+            text = "[emendoza264@unab.edu.co]",
+            color = Color.Blue,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp,0.dp)
+        )
+        Text(
+            text = "CIUDAD",
+            fontSize = 20.sp,
+            color = Color.Gray,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp,15.dp,25.dp,0.dp)
+        )
+        Text(
+            text = "Bucaramanga, Colombia",
+            fontSize = 20.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp,0.dp)
+        )
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = Color.Gray,
+            modifier = Modifier.padding(25.dp,25.dp,25.dp,15.dp)
+        )
+        Text(
+            text = "SOBRE MI MATERIA FAVORITA",
+            fontSize = 20.sp,
+            color = Color.Gray,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp)
+        )
+        Text(
+            text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(25.dp,0.dp)
+        )
+        Button(
+            modifier = Modifier
+                .padding(25.dp, 30.dp)
+                .fillMaxWidth(),
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Blue,
+                contentColor = Color.White
+            )
+        ) {
+            Text("Contactar conmigo")
+        }
 
 
 
