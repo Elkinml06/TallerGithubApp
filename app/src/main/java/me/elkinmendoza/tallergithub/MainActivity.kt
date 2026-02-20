0032b632638c915b4b7adf6b9d2c3c2dd984b24a
Elkin <elkin1506ml@gmail.com>
2026-02-20 08:47:23 -0500

    add image

---
+++ app/src/main/java/me/elkinmendoza/tallergithub/MainActivity.kt
@@ -0,0 +1,48 @@
+package me.elkinmendoza.tallergithub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.elkinmendoza.tallergithub.ui.theme.TallerGithubAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            presentacion()
+        }
+    }
+}
+
+@Preview(showBackground = true,
+    showSystemUi = true)
+@Composable
+fun presentacion() {
+    Column() {
+        val painter = painterResource(id = R.drawable.person)
+        Image(painter = painter,contentDescription =null)
+
+
+
+    }
+}
